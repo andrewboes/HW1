5bb6ef85745b7622c0b1dec35c7eb47bd6695827
Andrew <andrew.boes@gmail.com>
2023-01-20 13:54:52 -0800

    Update treeDepth.py

--- treeDepth.py
+++ treeDepth.py
@@ -8,3 +8,5 @@ class TreeNode:
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         return 0
+
+# Complete the Solutuion Method and also write a method to test this function.
